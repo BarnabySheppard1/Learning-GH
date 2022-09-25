58bd50a11836cf8637287bce7dd5bb92e7d194d7
BarnabySheppard1 <114286444+BarnabySheppard1@users.noreply.github.com>
2022-09-25 08:14:19 +0100

    Update ContentView to What's up

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, CodeWithChris")
+        Text("What's up, CodeWithChris")
             .padding()
     }
 }
